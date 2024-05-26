9e02e8e3bd645a2c577382121d8c1ae3333c8beb
Rodrigo <rodrigomesaw@gmail.com>
2024-05-25 21:22:07 -0300

    cambios

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.getElementById('invoiceForm').addEventListener('submit', function(event
     const customerName = document.getElementById('customerName').value;
     const phoneNumber = document.getElementById('phoneNumber').value;
     const invoiceDate = document.getElementById('invoiceDate').value;
+    const description = document.getElementById('description').value;
     
     const services = document.querySelectorAll('.service input[type="checkbox"]:checked');
     let total = 0;
@@ -28,24 +29,38 @@ document.getElementById('invoiceForm').addEventListener('submit', function(event
                 <link rel="stylesheet" type="text/css" href="styles_invoice.css">
             </head>
             <body>
-                <div class="img-info">
+                <div class="header">
                     <img src="./img/jslogo.png" alt="Logo de la Empresa">
                     <div class="company-info">
                         <p><strong>Empresa:</strong> JS Aires Pando</p>
                         <p><strong>Número:</strong> 093 365 696</p>
-                        <p><strong>Web:</strong> aireacondicionadopando.com</p>
+                        <p><strong>Web:</strong> Aireacondicionadopando.com</p>
                     </div>
+                    <p class="rut">RUT: 110374690018</p>
                 </div>
                 <h1>Factura</h1>
                 <div class="invoice-details">
                     <p><strong>Número de Factura:</strong> ${invoiceNumber}</p>
-                    <p><strong>Nombre del Cliente:</strong> ${customerName}</p>
-                    <p><strong>Número de Teléfono:</strong> ${phoneNumber}</p>
+                    <p><strong>Cliente:</strong> ${customerName}</p>
+                    <p><strong>Teléfono:</strong> ${phoneNumber}</p>
                     <p><strong>Fecha de Factura:</strong> ${invoiceDate}</p>
-                    ${serviceDetails}
-                    <p class="total"><strong>Total:</strong> $${total.toFixed(2)}</p>
                 </div>
-                <p class="authorized">Autorizado por: UTE</p>
+                <div class="service-details">
+                     ${serviceDetails}
+                </div>
+                <p><strong>Total:</strong> $${total.toFixed(2)}</p>
+                <div class="description">
+                    <p><strong>Detalles:</strong></p>
+                    <p>${description}</p>
+                </div>
+                <div class="footer">
+                    <div class="signature-space">
+                        <p>Jorge Silva</p>
+                    </div>
+                    <div class="future-text-space">
+                        <p>Autorizado bla bla / aaaaaaaaaaaaaaaaaa</p>
+                    </div>
+                </div>
             </body>
         </html>
     `;
@@ -54,3 +69,4 @@ document.getElementById('invoiceForm').addEventListener('submit', function(event
     newWindow.document.write(newWindowContent);
     newWindow.document.close();
 });
+
